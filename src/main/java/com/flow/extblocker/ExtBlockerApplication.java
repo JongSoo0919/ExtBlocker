e6c31f8cb7f32377e459263596e6399174026d14
JongSooKim <prjs3643@gmail.com>
2023-08-23 17:43:44 +0900

    Initial commit

---
+++ src/main/java/com/flow/extblocker/ExtBlockerApplication.java
@@ -0,0 +1,13 @@
+package com.flow.extblocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtBlockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtBlockerApplication.class, args);
+	}
+
+}
